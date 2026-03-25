6690bbb7fedf51ac5304112314382af69f83ea55
Norbert Takacs <norbert.takacs@gmail.com>
2026-03-25 21:10:53 +0100

    Initial commi

---
+++ src/main/java/com/nbrt/dtracing/DtracingApplication.java
@@ -0,0 +1,13 @@
+package com.nbrt.dtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtracingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbrt/dtracing/DtracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbrt.dtracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
